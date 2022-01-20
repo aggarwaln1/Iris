91a589c7057b9623106ff079c7ba43d490f750a9
aggarwaln1 <gusain.nidhi@gmail.com>
2022-01-20 18:16:34 +0530

    Code Added and template

---
+++ src/app.py
@@ -0,0 +1,52 @@
+
+from flask import Flask, make_response, request,render_template
+import io
+from io import StringIO
+import csv
+import pandas as pd
+import numpy as np
+import pickle
+import os
+
+filename =r'E:\Nidhi\MLOps training\MLOps_Code\Practice_ML\Iris\saved_models\model_rfc.pickle'
+os.chdir(r'E:\Nidhi\MLOps training\MLOps_Code\Practice_ML\Iris')
+
+app= Flask(__name__, template_folder='template')
+def transform(text_file_contents):
+    return text_file_contents.replace("=", ",")
+
+
+@app.route('/')
+def main():
+    return render_template('home.html')
+
+@app.route('/transform', methods=["POST"])
+def transform_view():
+    f = request.files['data_file']
+    if not f:
+        return "No file"
+
+    stream = io.StringIO(f.stream.read().decode("UTF8"), newline=None)
+    csv_input = csv.reader(stream)
+    for row in csv_input:
+        print(row)
+
+    stream.seek(0)
+    result = transform(stream.read())
+    df = pd.read_csv(StringIO(result))
+    df = df.drop(['Id'],axis=1)
+
+    # load the model from disk
+    load_model = pickle.load(open(filename, 'rb'))
+    df['prediction'] = load_model.predict(df)
+
+    
+
+    return render_template('simple.html',  tables=[df.to_html(classes='data')], titles=df.columns.values)
+
+
+if __name__ =='__main__':
+    from werkzeug.serving import run_simple
+    app.run(debug=True)
+
+  
--- src/iris_model.py
+++ src/iris_model.py
@@ -3,3 +3,31 @@ from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import train_test_split
 import pickle
 import os
+import pandas as pd
+
+iris = pd.read_csv(r'E:\Nidhi\MLOps training\MLOps_Code\Practice_ML\Iris\data\Iris.csv')
+iris = iris.drop(['Id'],axis=1)
+
+print(iris.head())
+
+# our main data split into train and test
+# the attribute test_size=0.3 splits the data into 70% and 30% ratio. train=70% and test=30%
+train, test = train_test_split(iris, test_size=0.3) 
+print(train.shape)
+print(test.shape)
+
+train_X =train.iloc[:,:3]
+train_y =train.iloc[:,-1]
+
+test_X =test.iloc[:,:3]
+test_y =test.iloc[:,-1]
+
+rfc =RandomForestClassifier(n_estimators=500)
+rfc.fit(train_X, train_y)
+
+filename =r'E:\Nidhi\MLOps training\MLOps_Code\Practice_ML\Iris\saved_models\model_rfc.pickle'
+pickle.dump(rfc,open(filename,'wb'))
+
+load_model =pickle.load(open(filename,'rb'))
+result = load_model.score(test_X,test_y)
+print(result)
